8b1749900ae3f4446010868fda475d40a4286ca9
tengo_liu <liu5087120!@163.com>
2018-12-28 18:09:31 +0800

    微调

--- src/tengo/spring/controller/HomeController.java
+++ src/tengo/spring/controller/HomeController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/")
 public class HomeController {
 
-	@RequestMapping("/index")
+	@RequestMapping({ "/", "index" })
 	public String index() {
 		return "home_index";
+	}
+
+	@RequestMapping({ "about" })
+	public String about() {
+		return "home_about";
 	}
 }
